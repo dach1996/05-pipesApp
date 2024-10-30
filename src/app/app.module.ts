b08381c8c42c66c60e50234407c81b60da608dee
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-10-30 17:56:29 -0500

    cap 160

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,7 +6,12 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsModule } from './products/products.module';
+import localEsEc from '@angular/common/locales/es-EC';
+import localfrCa from '@angular/common/locales/fr-CA';
+import { registerLocaleData } from '@angular/common';
 
+registerLocaleData(localEsEc);
+registerLocaleData(localfrCa);
 @NgModule({
   declarations: [
     AppComponent
@@ -19,7 +24,9 @@ import { ProductsModule } from './products/products.module';
     BrowserAnimationsModule,
     ProductsModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-EC'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
