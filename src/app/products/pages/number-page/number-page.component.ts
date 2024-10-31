b0acef5ef28a6ef5e02ea16b5c0e49bfde2a477d
Danny Cardenas <danny.cardenas@bayteq.com>
2024-10-30 22:31:18 -0500

    cap 161

--- src/app/products/pages/number-page/number-page.component.ts
+++ src/app/products/pages/number-page/number-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './number-page.component.css'
 })
 export class NumberPageComponent {
-
+ public totalSells: number = 234324234.3423;
+ public percent: number = 0.738;
 }
