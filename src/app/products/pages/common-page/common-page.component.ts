09de2cf468016508636db912255f0cdf29a22133
Danny Cardenas <danny.cardenas@bayteq.com>
2024-10-30 22:56:09 -0500

    cap 165

--- src/app/products/pages/common-page/common-page.component.ts
+++ src/app/products/pages/common-page/common-page.component.ts
@@ -6,18 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './common-page.component.css'
 })
 export class CommonPageComponent {
+  deleteClient() {
+    this.clients.pop();
+  }
   changeClient() {
     this.name = 'Melisa';
     this.gender = 'Female';
   }
 
-  public invatationMap ={
-    'Male':'Invitarlo',
-    'Female':'Invitarla'
+  public invatationMap = {
+    'Male': 'Invitarlo',
+    'Female': 'Invitarla'
   }
 
   public name: string = 'Fernando';
   public gender: 'Male' | 'Female' = 'Male';
 
+  public clients: string[] = ['María', 'Perdro', 'Ferando', 'Hernando', 'Meliza'];
+  public clientsMap = {
+    '=0': 'No tenemos ningun cliente esperando',
+    '=1': 'Tenemos 1 cliente esperando',
+    'other': "Tenemos # clientes esperando"
+  };
 
 }
